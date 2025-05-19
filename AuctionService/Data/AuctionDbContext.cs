48d1b2a47285f53487fbee48bd06892a6136ec43
Javad <javad0470@gmail.com>
2025-05-20 00:54:21 +0330

    section 2

---
+++ AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+// Migration Command in working Solution (here AuctionService)
+// dotnet ef migrations add "Initial-Create" -o Data/Migrations
+
+public class AuctionDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Auction> Auctions { get; set; }
+}
---
+++ AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+// profile belong to auto mapper
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(destination =>
+                    destination.Item,
+                opt => opt.MapFrom(source => source)
+            );
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
