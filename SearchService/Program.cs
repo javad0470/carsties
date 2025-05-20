0d384c1f79d5da3dada0f9dfa0b098b96278e187
Javad <javad0470@gmail.com>
2025-05-20 15:58:51 +0330

    add search service

---
+++ SearchService/Program.cs
@@ -0,0 +1,30 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
